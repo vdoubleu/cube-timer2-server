86b380a944cd928589c27ddd216db8e7589d6a8a
Victor Wang <victorwang2001@gmail.com>
2020-07-26 14:34:24 -0400

    more test

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def test():
-    return jsonify("hello")
+    name = request.args.get("name", None)
+
+    return jsonify(name)
 
 if __name__ == '__main__':
     app.run();
