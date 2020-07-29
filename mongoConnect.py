24d14c93463bd0e4d9b9f1a1c73bfb6e43a92244
Victor Wang <victorwang2001@gmail.com>
2020-07-28 20:36:08 -0400

    changed mongo env

--- mongoConnect.py
+++ mongoConnect.py
@@ -5,8 +5,7 @@ def connect():
     user = os.getenv("DBUSER")
     pw = os.getenv("DBPW")
 
-    #uri = "mongodb+srv://" + user + ":" + pw + "@cluster0.gnkvv.mongodb.net/test?retryWrites=true&w=majority"
-    uri = "mongodb+srv://vdoubleu:passwordVW12345@cluster0.gnkvv.mongodb.net/test?retryWrites=true&w=majority"
+    uri = "mongodb+srv://" + user + ":" + pw + "@cluster0.gnkvv.mongodb.net/test?retryWrites=true&w=majority"
     client = MongoClient(uri)
 
     db = client.test
